926149c44836ed97a9720d19e7fc53012901c0fb
GV2005 <girivenkatesh2005@gmail.com>
2026-06-14 17:05:33 +0530

    Completed Hospital Management System

--- hospital_project/hospital_project/settings.py
+++ hospital_project/hospital_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&z-bpw!&xcge&wh1+x#k054o010!!-0t!rcn!z6x77%xacn&wm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 LOGIN_URL="login"
 
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
